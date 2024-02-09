7b6b56eaa98f49b92ce31ba9f6d312ad3db23d5c
Murodali Narzullaev <murodalinarzullaevofficial@gmail.com>
2024-02-09 15:23:39 +0500

    modified

--- handlers/users/help.py
+++ handlers/users/help.py
@@ -6,7 +6,7 @@ from loader import dp
 
 @dp.message_handler(CommandHelp())
 async def bot_help(message: types.Message):
-    text = ("Buyruqlar: ",
+    text = ("Commands: ",
             "/start - Run the bot",
             "/help - Some info about bot")
     
